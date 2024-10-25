3342581e0cb935110b5fc37d584b20bf026b2acf
wildemberg-sales <junior_sales2010@hotmail.com>
2024-10-25 17:05:00 -0300

    Adicionando correção na execução dos containers

--- todo-list-api/setup.sh
+++ todo-list-api/setup.sh
@@ -7,5 +7,12 @@ done
 
 echo "PostgreSQL is ready, running migrations..."
 rails db:migrate
+
+# Remove stale PID file if it exists
+if [ -f /api/tmp/pids/server.pid ]; then
+    echo "Removing stale PID file."
+    rm /api/tmp/pids/server.pid
+fi
+
 echo "Starting the application..."
 bundle exec rails s -p 3000 -b '0.0.0.0'
